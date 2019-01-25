63740c45d3adbe133e890254219ac6fa84bd78e1
Chris Franklin <cf73021@gmail.com>
2019-01-25 02:41:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,8 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {return "You are a teenager!"} 
+  else if {return "You are not a teenager"}
 }
 
 function ternaryTeenager(age) {
