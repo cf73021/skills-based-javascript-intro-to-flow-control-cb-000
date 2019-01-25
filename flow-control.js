96074b9f3814dfa9024a60eb858f4f7119226fd0
Chris Franklin <cf73021@gmail.com>
2019-01-25 02:57:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,14 +24,14 @@ function switchAge(age) {
       return "You are a teenager"
     case 15:
       return "You are a teenager"
-      case 15:
-        return "You are a teenager"
-        case 15:
-          return "You are a teenager"
-          case 15:
-            return "You are a teenager"
-            case 15:
-              return "You are a teenager"
+    case 16:
+      return "You are a teenager"
+    case 17:
+      return "You are a teenager"
+    case 18:
+      return "You are a teenager"
+    case 19:
+      return "You are a teenager"
     default:
       return "You have an age"
   }
